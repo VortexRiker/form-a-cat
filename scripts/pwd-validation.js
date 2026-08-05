24279cb5b39bba88fd362ce0c50f0623af9d87fc
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-08-05 16:31:52 +0500

    Add helper functions

--- scripts/pwd-validation.js
+++ scripts/pwd-validation.js
@@ -1,9 +1,14 @@
+function isValid(password, confirmation)
+{
+    return (password.value !== confirmation.value) || !password.validity.valid;
+}
+
 function validatePasswords()
 {
     const password = document.getElementById("password");
     const confirmation = document.getElementById("password-confirm");
 
-    if((password.value !== confirmation.value) || !password.validity.valid)
+    if(isValid(password, confirmation))
     {
        confirmation.setCustomValidity("Passwords do not match");
     }
@@ -13,7 +18,25 @@ function validatePasswords()
     }
 }
 
-const password = document.getElementById("password");
-password.addEventListener("change", validatePasswords);
-const confirmation = document.getElementById("password-confirm");
-confirmation.addEventListener("change", validatePasswords);
+function addPasswordListener()
+{
+    const password = document.getElementById("password");
+    password.addEventListener("change", validatePasswords);
+}
+
+function addConfirmationListener()
+{
+    const confirmation = document.getElementById("password-confirm");
+    confirmation.addEventListener("change", validatePasswords);
+}
+
+function addValidationListeners()
+{
+   addPasswordListener();
+   addConfirmationListener(); 
+}
+
+addValidationListeners();
+
+
+
